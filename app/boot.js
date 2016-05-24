fdd5252cdd6d6ed266195106a2741589f978d926
Marcin Maslanka <maslankam92@gmail.com>
2016-05-24 19:08:09 +0200

    init

---
+++ app/boot.js
@@ -0,0 +1,20 @@
+System.register(["@angular/platform-browser-dynamic", "./app.component"], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    var platform_browser_dynamic_1, app_component_1;
+    return {
+        setters:[
+            function (platform_browser_dynamic_1_1) {
+                platform_browser_dynamic_1 = platform_browser_dynamic_1_1;
+            },
+            function (app_component_1_1) {
+                app_component_1 = app_component_1_1;
+            }],
+        execute: function() {
+            //noinspection TypeScriptValidateTypes
+            platform_browser_dynamic_1.bootstrap(app_component_1.AppComponent);
+        }
+    }
+});
+
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImJvb3QudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7Ozs7OztZQUlBLHNDQUFzQztZQUN0QyxvQ0FBUyxDQUFDLDRCQUFZLENBQUMsQ0FBQyIsImZpbGUiOiJib290LmpzIiwic291cmNlc0NvbnRlbnQiOlsiLy8vPHJlZmVyZW5jZSBwYXRoPVwiLi4vdHlwaW5ncy9icm93c2VyLmQudHNcIi8+XG5pbXBvcnQge2Jvb3RzdHJhcH0gZnJvbSBcIkBhbmd1bGFyL3BsYXRmb3JtLWJyb3dzZXItZHluYW1pY1wiO1xuaW1wb3J0IHtBcHBDb21wb25lbnR9IGZyb20gXCIuL2FwcC5jb21wb25lbnRcIjtcblxuLy9ub2luc3BlY3Rpb24gVHlwZVNjcmlwdFZhbGlkYXRlVHlwZXNcbmJvb3RzdHJhcChBcHBDb21wb25lbnQpOyJdLCJzb3VyY2VSb290IjoiL3NvdXJjZS8ifQ==
--- dev/app.component.ts
+++ dev/app.component.ts
@@ -1,30 +1,9 @@
 import {Component} from '@angular/core';
-import {RecipesComponent} from "./recipe-book/recipes.component";
-import {Routes} from "@angular/router";
-import {ShoppingListComponent} from "./shopping-list/shopping-list.component";
-import {ROUTER_DIRECTIVES} from "@angular/router";
 
 @Component({
     selector: 'my-app',
-    template: `
-        <header>
-            <nav>
-                <ul>
-                    <li><a [routerLink]="['recipes']">Recipes</a></li>
-                    <li><a [routerLink]="['shopping-list']">Shopping List</a></li>
-                </ul>
-            </nav>
-        </header>
-        <div class="main">
-            <router-outlet></router-outlet>
-        </div>
-    `,
-    directives: [ROUTER_DIRECTIVES]
+    template: 'Hello World'
 })
-@Routes([
-    {path: '/recipes', component: RecipesComponent}, // useAsDefault: true => coming soon
-    {path: '/shopping-list', component: ShoppingListComponent},
-])
-export class AppComponent {
 
+export class AppComponent {
 }
