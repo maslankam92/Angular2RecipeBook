32d9534482c6cfdaf9195a86cb531137531cb5d7
Marcin Maślanka <maslankam92@gmail.com>
2016-05-30 20:41:32 +0200

    Added Shopping list

--- dev/shopping-list/shopping-list.component.ts
+++ dev/shopping-list/shopping-list.component.ts
@@ -1,18 +1,34 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { ShoppingListEditComponent } from "./shopping-list-edit.component";
+import { Ingredient } from "../shared/ingredient";
+import { ShoppingListService } from "../shared/shopping-list.service";
 
 @Component({
     selector: 'my-shopping-list',
     template: `
         <h1>Shopping List</h1>
-        <div>Edit Section</div>
+        <my-shopping-list-edit [ingredient]="selectedItem"></my-shopping-list-edit>
         <div class="list">
             <button class="btn">Add new Item</button>
             <ul>
-                <li>Lisdt Items...</li>
+                <li *ngFor="let item of shoppingList" (click)="onSelectItem(item)">{{item.name}} ({{item.amount}})</li>
             </ul>
         </div>
-    `
+    `,
+    directives: [ShoppingListEditComponent],
+    providers: [ShoppingListService]
 })
-export class ShoppingListComponent {
+export class ShoppingListComponent implements OnInit {
+    shoppingList: Ingredient[];
+    selectedItem: Ingredient = null;
 
+    constructor(private _shoppingListService: ShoppingListService) {}
+
+    ngOnInit():any {
+        this.shoppingList = this._shoppingListService.getAllItems();
+    }
+
+    onSelectItem(item: Ingredient) {
+        this.selectedItem = item;
+    }
 }
